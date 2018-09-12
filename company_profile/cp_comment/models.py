c6c0cb7a075c95523e94d463a8555ab1fc131162
ryosuwito <ryosuwito.grades@gmail.com>
2018-09-12 16:05:22 +0700

    add comment models

--- company_profile/cp_comment/models.py
+++ company_profile/cp_comment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from company_profile.cp_articles.models import Article
 
-# Create your models here.
+class Visitor(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, related_name="article_comment", on_delete=models.CASCADE)
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, related_name="comment_reply", on_delete=models.CASCADE)
