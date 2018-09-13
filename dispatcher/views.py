86ac567cfc1788e01512aba7ead8d71811b83d9b
ryosuwito <ryosuwito.grades@gmail.com>
2018-09-13 13:04:37 +0700

    attach comment to blog post

--- dispatcher/views.py
+++ dispatcher/views.py
@@ -95,7 +95,9 @@ class Article(Dispatcher):
 
         if site.domain == 'sidomo.com':
             template = "zeon_backend/templates/blog-post.html"
-            return render(request, template, {'article': article})
+            comment = Comment()
+            comment_and_reply = list(comment.get_comment_and_reply(article))
+            return render(request, template, {'article': article, 'comment':comment_and_reply})
 
         assets = configs.brand_assets
         scheme = configs.color_scheme
