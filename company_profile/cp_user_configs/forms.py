77d0ffb27aaf0b7a1abee6357526a9498023d6eb
ryosuwito <ryosuwito.grades@gmail.com>
2018-09-17 10:59:41 +0700

    change template

--- company_profile/cp_user_configs/forms.py
+++ company_profile/cp_user_configs/forms.py
@@ -4,3 +4,13 @@ from company_profile.cp_configs.models import BrandAsset
 class AssetEditForm(forms.ModelForm):
     class Meta:
         model = BrandAsset
+        fields = ('favicon',
+                    'hero_image_1',
+                    'hero_image_2',
+                    'hero_image_3',
+                    'brand_logo',
+                    'main_photo_1',
+                    'main_photo_2',
+                    'extra_image_1',
+                    'extra_image_2',
+                    'extra_image_3',)
