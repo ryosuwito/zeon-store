bee9d9e1f6830fc2da22c7d2560fbf71f9122c0f
ryosuwito <ryosuwito.grades@gmail.com>
2018-09-20 20:02:49 +0700

    recent_post

--- company_profile/cp_articles/views.py
+++ company_profile/cp_articles/views.py
@@ -36,10 +36,10 @@ class CPArticle(LoginRequiredMixin, ComponentRenderer, Dispatcher):
             self.form = ArticleAddForm(request.POST, request.FILES, instance=article)
             if self.form.is_valid():
                 article = self.form.save()
-                return HttpResponse(article.category.all())
                 if not article.category.all() :
                     article.category.add(Category.objects.get_or_create(site=site, title="post")[0])
                     article.save()
+                return HttpResponse(article.category.all())
                 return HttpResponseRedirect(self.index_url)
         elif  kwargs['action'] == 'preview':
             self.form = ArticlePreviewForm(request.POST, request.FILES)
