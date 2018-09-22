dcd7042b7c0138ae66785e0b20a8b081c6f33c25
ryosuwito <ryosuwito.grades@gmail.com>
2018-09-22 09:40:58 +0700

    recent_post

--- company_profile/cp_comment/views.py
+++ company_profile/cp_comment/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from dispatcher.views import ComponentRenderer
 from dispatcher.views import Dispatcher
+
+from .models import Comment, Reply, Visitor
+from .forns import 
       
 class CPComment(LoginRequiredMixin, ComponentRenderer, Dispatcher):
     login_url = '/cms/login/'
@@ -9,14 +12,14 @@ class CPComment(LoginRequiredMixin, ComponentRenderer, Dispatcher):
     component = {}
     component['base'] = 'cp_admin/component/index_base.html'
     component['header'] =  'cp_admin/component/index_header.html'
-    index_main = 'cp_articles/component/cms_category_main.html'
-    index_local_script = 'cp_articles/component/cms_category_local_script.html'
-    add_main = 'cp_articles/component/cms_category_add_main.html'
-    add_local_script = 'cp_articles/component/cms_category_add_local_script.html'
-    edit_main = 'cp_articles/component/cms_category_edit_main.html'
-    edit_local_script = 'cp_articles/component/cms_category_edit_local_script.html'
-    index_url = '/cms/category/'
-    #form = CategoryAddForm()
+    index_main = 'cp_articles/component/cms_comment_main.html'
+    index_local_script = 'cp_articles/component/cms_comment_local_script.html'
+    add_main = 'cp_articles/component/cms_comment_add_main.html'
+    add_local_script = 'cp_articles/component/cms_ommenty_add_local_script.html'
+    edit_main = 'cp_articles/component/cms_comment_edit_main.html'
+    edit_local_script = 'cp_articles/component/cms_comment_edit_local_script.html'
+    index_url = '/cms/comment/'
+    form = AddCommentForm()
 
     def post(self, request, *args, **kwargs):
         pass
@@ -53,44 +56,31 @@ class CPComment(LoginRequiredMixin, ComponentRenderer, Dispatcher):
         )"""
 
     def get(self, request, *args, **kwargs):
-        pass
-        """category = ""
+        comment = ""
         if  kwargs['action'] == 'delete':
             if kwargs['pk'] == 'none':
                 return HttpResponseRedirect(self.index_url)
             else :
                 try:
-                    category = Category.objects.get(pk=kwargs['pk'])
+                    comment = Comment.objects.get(pk=kwargs['pk'])
+                    comment.delete()
                 except:
                     return HttpResponseRedirect(self.index_url)
-        if category.title == 'post':
-            return HttpResponseRedirect(self.index_url)
 
         method = request.GET.get('method', '')
-        data = super(CPCategory, self).get(request, args, kwargs)
+        data = super(CPComment, self).get(request, args, kwargs)
         token = get_token(request)
         member = data['member']
         configs = UserConfigs.objects.get(member = member)
         site = data['site']
         form = self.form
-        featured_image = ''
         if  kwargs['action'] == 'edit':
             try:
-                category = Category.objects.get(pk=kwargs['pk'])
+                comment = Comment.objects.get(pk=kwargs['pk'])
             except:
                 return HttpResponseRedirect(self.index_url)
-            form = CategoryAddForm(instance=category)
+            form = AddCommentForm(instance=comment)
 
-        elif  kwargs['action'] == 'delete':
-            articles = ArticleModel.objects.filter(category=category)
-            if articles:
-                for article in articles:
-                    try:
-                        article.category.remove(category)
-                    except:
-                        pass
-            category.delete()
-            return HttpResponseRedirect(self.index_url)
 
         if kwargs['action'] == 'show_all' or \
             kwargs['action'] == 'add' or \
@@ -99,7 +89,8 @@ class CPComment(LoginRequiredMixin, ComponentRenderer, Dispatcher):
             self.set_component(kwargs)
         else :
             return HttpResponseRedirect(self.index_url)
-
+            
+        featured_image = ""
         if method == 'get_component':
             return self.get_component(request, token, data, configs, site, member, form, featured_image)
                                      
@@ -113,7 +104,7 @@ class CPComment(LoginRequiredMixin, ComponentRenderer, Dispatcher):
                 'token': token,
                 'component':self.component,
             }
-        )"""
+        )
 
 class CPReply(LoginRequiredMixin, ComponentRenderer, Dispatcher):
     login_url = '/cms/login/'
