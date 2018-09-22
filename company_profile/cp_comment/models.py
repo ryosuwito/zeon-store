e2991967192e824c3794aa053e35196062171fb8
ryosuwito <ryosuwito.grades@gmail.com>
2018-09-22 09:58:57 +0700

    recent_post

--- company_profile/cp_comment/models.py
+++ company_profile/cp_comment/models.py
@@ -1,3 +1,4 @@
+from django.contrib.sites.models import Site
 from django.db import models
 from company_profile.cp_articles.models import Article
 from django.utils import timezone
@@ -10,12 +11,14 @@ class Visitor(models.Model):
     created_date = models.DateTimeField(db_index=True,default=default_now)
     name = models.CharField(max_length=50)
     email = models.EmailField()
-
+    site = models.ForeignKey(Site, on_delete=models.CASCADE,related_name='visitor_site', null=True, blank=True)
+    
 class Comment(models.Model):
     created_date = models.DateTimeField(db_index=True,default=default_now)
     visitor = models.ForeignKey(Visitor, on_delete=models.CASCADE, null=True)
     article = models.ForeignKey(Article, related_name="article_comment", on_delete=models.CASCADE)
     content = models.TextField(null=True)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE,related_name='comment_site', null=True, blank=True)
 
 class Reply(models.Model):
     created_date = models.DateTimeField(db_index=True,default=default_now)
