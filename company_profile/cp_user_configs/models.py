038766959f7fe863a5f20003784b86ffeae5b986
ryosuwito <ryosuwito.grades@gmail.com>
2018-09-28 18:12:50 +0700

    add_custom_form

--- company_profile/cp_user_configs/models.py
+++ company_profile/cp_user_configs/models.py
@@ -1,9 +1,10 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 from membership.models import Member
 from company_profile.cp_configs.models import Template, ColorScheme, BrandAsset, BrandIdentity
 
 class UserConfigs(models.Model):
-    member = models.OneToOneField(Member, on_delete=models.SET_NULL, null=True, blank=True)
+    member = models.OneToOneField(Member, on_delete=models.CASCADE, null=True, blank=True)
     templates = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True, blank=True)
     color_scheme = models.ForeignKey(ColorScheme, on_delete=models.SET_NULL, null=True, blank=True)
     brand_assets = models.ForeignKey(BrandAsset, on_delete=models.SET_NULL, null=True, blank=True)
@@ -13,3 +14,21 @@ class UserConfigs(models.Model):
 
     class Meta:
         verbose_name_plural = "User Configs"
+
+class UserFormTemplate(models.Model):
+    TYPE_TEXT = 'text'
+    TYPE_EMAIL = 'email'
+    TYPE_CHECKBOX = 'checkbox'
+    TYPE_RADIO = 'radio'
+    TYPE_NUMBER = 'number'
+    TEXTAREA = 'textarea'
+    member = models.OneToOneField(Member, on_delete=models.CASCADE, null=True, blank=True)
+    data = JSONField() 
+    def __str__(self):
+        return self.member.user.username.title()
+
+class UserFormData(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, null=True, blank=True)
+    data = JSONField() 
+    def __str__(self):
+        return self.member.user.username.title()
