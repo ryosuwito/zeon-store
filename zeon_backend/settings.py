760c2bca2b8ba8161f72839f52cd0e4ad25d603c
ryosuwito <ryosuwito.grades@gmail.com>
2018-09-29 10:13:53 +0700

    clone

--- zeon_backend/settings.py
+++ zeon_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dli3^=1mdk6vj-d+k*e8=49o^g35gqhc7_3)l4m^++wa^h)b8j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['178.128.115.116',
+ALLOWED_HOSTS = [
                 'trials.sidomo.com',
                 '.co.id',
                 '.com',
