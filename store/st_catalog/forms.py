fbc83012393ce7bf3d5701a51bd518815aaafd78
ryosuwito <ryosuwito.grades@gmail.com>
2018-10-19 14:04:10 +0700

    urls

--- store/st_catalog/forms.py
+++ store/st_catalog/forms.py
@@ -12,11 +12,16 @@ class CategoryAddForm(forms.ModelForm):
 class ProductAddForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ProductAddForm, self).__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs['rows'] = '3'
+        self.fields['description'].widget.attrs['style'] = 'width:100%; padding:10px'
+        self.fields['description'].widget.attrs['placeholder'] = ''
         self.fields['categories'].help_text = 'Pilih Kategori Artikel *(bisa lebih dari 1)'
         self.fields['categories'].widget.attrs['style'] = 'width:100%; padding:10px'
         self.fields['name'].widget.attrs['style'] = 'width:100%; padding:10px'
         self.fields['is_available'].widget.attrs['style'] = 'margin:10px 10px 0 0'
         self.fields['is_available'].widget.attrs['data-toggle'] = 'toggle'
+        self.fields['is_archived'].widget.attrs['style'] = 'margin:10px 10px 0 0'
+        self.fields['is_archived'].widget.attrs['data-toggle'] = 'toggle'
         self.fields['price'].widget.attrs['style'] = 'width:100%; padding:10px'
         self.fields['unit_weight'].widget.attrs['style'] = 'width:100%; padding:10px'
         
