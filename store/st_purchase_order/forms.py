b60dbbfc56761776d750849af844b90b3fa7cb90
ryosuwito <ryosuwito.grades@gmail.com>
2018-10-23 15:45:23 +0700

    change_templates

--- store/st_purchase_order/forms.py
+++ store/st_purchase_order/forms.py
@@ -6,12 +6,24 @@ class OrderAddForm(forms.ModelForm):
         super(OrderAddForm, self).__init__(*args, **kwargs)
         self.fields['is_verified'].widget.attrs['style'] = 'margin:10px 10px 0 0'
         self.fields['is_verified'].widget.attrs['data-toggle'] = 'toggle'
-        
+        self.fields['is_verified'].widget.attrs['data-on'] = 'Yes'
+        self.fields['is_verified'].widget.attrs['data-off'] = 'No'
+        self.fields['is_verified'].widget.attrs['data-onstyle'] = 'success'
+        self.fields['is_verified'].widget.attrs['data-offstyle'] = 'danger'
+
         self.fields['is_valid'].widget.attrs['style'] = 'margin:10px 10px 0 0'
         self.fields['is_valid'].widget.attrs['data-toggle'] = 'toggle'
+        self.fields['is_valid'].widget.attrs['data-on'] = 'Yes'
+        self.fields['is_valid'].widget.attrs['data-off'] = 'No'
+        self.fields['is_valid'].widget.attrs['data-onstyle'] = 'success'
+        self.fields['is_valid'].widget.attrs['data-offstyle'] = 'danger'
 
         self.fields['is_paid'].widget.attrs['style'] = 'margin:10px 10px 0 0'
         self.fields['is_paid'].widget.attrs['data-toggle'] = 'toggle'
+        self.fields['is_paid'].widget.attrs['data-on'] = 'Yes'
+        self.fields['is_paid'].widget.attrs['data-off'] = 'No'
+        self.fields['is_paid'].widget.attrs['data-onstyle'] = 'success'
+        self.fields['is_paid'].widget.attrs['data-offstyle'] = 'danger'
 
     class Meta:
         model = PurchaseOrder
