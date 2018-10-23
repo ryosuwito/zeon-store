88d51cf9b6b6becd732ca560efb7b2f26ca72016
ryosuwito <ryosuwito.grades@gmail.com>
2018-10-23 14:12:26 +0700

    change_templates

--- store/st_customer/admin.py
+++ store/st_customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    model = Customer
+    
+admin.site.register(Customer, CustomerAdmin)
